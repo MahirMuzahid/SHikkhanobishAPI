7f1668d0c183d830d9ffb05dee4f7787e735bb96
Mahir Muzahid <muzahid12@live.com>
2020-08-03 19:48:53 +0600

    Update for teacher reg

--- Models/Medhabi/PlayerHistory.cs
+++ Models/Medhabi/PlayerHistory.cs
@@ -7,13 +7,13 @@ namespace SHikkhanobishAPI.Models.Medhabi
 {
     public class PlayerHistory
     {
-        public int matchID { get; set; }
+        public string matchID { get; set; }
         public string matchStatus { get; set; }
-        public int q1 { get; set; }
-        public int q2 { get; set; }
-        public int q3 { get; set; }
-        public int q4 { get; set; }
-        public int q5 { get; set; }
+        public string q1 { get; set; }
+        public string q2 { get; set; }
+        public string q3 { get; set; }
+        public string q4 { get; set; }
+        public string q5 { get; set; }
         public int playerID { get; set; }
         public int whatToDO { get; set; }
         public string response { get; set; }
