2ee31c71df41f8a846903c6c335381d403585517
Mahir <mahirmuzahid@gmail.com>
2021-03-23 02:54:27 +0600

    New Added

---
+++ Controllers/ApiMakerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SHikkhanobishAPI.Controllers
+{
+    public class ApiMakerController : Controller
+    {
+        private SqlConnection conn;
+        public void Connection()
+        {
+            string conString = ConfigurationManager.ConnectionStrings["getConnection"].ToString();
+            conn = new SqlConnection(conString);
+        }
+    }
+}
